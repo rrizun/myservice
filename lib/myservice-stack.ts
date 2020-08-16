c1ff306bb035a2fa186aa7ca247e560d609ee981
rrizun <rrizun@MacBook-Air.local>
2020-08-16 09:06:40 -0700

    create a topic

--- lib/myservice-stack.ts
+++ lib/myservice-stack.ts
@@ -1,9 +1,11 @@
 import * as cdk from '@aws-cdk/core';
+import * as sns from '@aws-cdk/aws-sns';
 
 export class MyserviceStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    new sns.Topic(this, "Topic")
   }
 }
