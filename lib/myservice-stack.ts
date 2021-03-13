e1291fb7217a86a5a9e6a5ef3c75855b3048a16f
rrizun <rrizun@rrizuns-MacBook-Air.local>
2021-03-13 07:02:05 -0800

    add bastion

--- lib/myservice-stack.ts
+++ lib/myservice-stack.ts
@@ -1,4 +1,6 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as iam from '@aws-cdk/aws-iam';
 import * as sns from '@aws-cdk/aws-sns';
 import * as sqs from '@aws-cdk/aws-sqs';
 import * as subscriptions from '@aws-cdk/aws-sns-subscriptions';
@@ -15,6 +17,13 @@ export class MyserviceStack extends cdk.Stack {
     const myTopic = new sns.Topic(this, "MyTopic")
     const myQueue = new sqs.Queue(this, "MyQueue");
     myTopic.addSubscription(new subscriptions.SqsSubscription(myQueue));
+
+    const vpc = new ec2.Vpc(this, "MyVpc", {maxAzs:1})
+    const instanceType = ec2.InstanceType.of(ec2.InstanceClass.STANDARD5, ec2.InstanceSize.LARGE);
+    const host = new ec2.BastionHostLinux(this, 'BastionHost', { vpc, instanceType });
+
+    const powerUserAccess = iam.ManagedPolicy.fromManagedPolicyArn(this, "PowerUserAccess", "arn:aws:iam::aws:policy/PowerUserAccess")
+    host.role.addManagedPolicy(powerUserAccess)
 
     this.urlOutput = new CfnOutput(this, 'myTopicArn', {
       value: myTopic.topicArn,
