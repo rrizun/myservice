8030a7853988238bffa425567e72fc3c81475987
rrizun <rrizun@rrizuns-MacBook-Air.local>
2021-03-21 15:49:22 -0700

    $@

--- cdkdev.sh
+++ cdkdev.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -ex
 # https://aws.amazon.com/blogs/developer/cdk-pipelines-continuous-delivery-for-aws-cdk-applications/
-CDKCOMMAND=${1?}
 npm run build # If necessary, to recompile the Lambda sources
 npx cdk synth
-npx cdk -a cdk.out/assembly-Dev ${CDKCOMMAND?}
+npx cdk -a cdk.out/assembly-Dev "$@"
